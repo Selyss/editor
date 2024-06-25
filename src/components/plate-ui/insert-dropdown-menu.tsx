51e6f76eff3ea1cc07c7533db2d6bf3c464eb3c8
Selyss <99344963+Selyss@users.noreply.github.com>
2024-06-25 20:20:32 +0000

    add more fixed toolbar features

--- src/components/plate-ui/insert-dropdown-menu.tsx
+++ src/components/plate-ui/insert-dropdown-menu.tsx
@@ -23,6 +23,12 @@ import {
   useOpenState,
 } from './dropdown-menu';
 import { ToolbarButton } from './toolbar';
+import { ELEMENT_EXCALIDRAW } from '@udecode/plate-excalidraw';
+import { ELEMENT_IMAGE, ELEMENT_MEDIA_EMBED } from '@udecode/plate-media';
+import { ELEMENT_CODE_BLOCK } from '@udecode/plate-code-block';
+import { ELEMENT_LINK } from '@udecode/plate-link';
+import { ELEMENT_TABLE } from '@udecode/plate-table';
+import { ELEMENT_HR } from '@udecode/plate-horizontal-rule';
 
 const items = [
   {
@@ -58,72 +64,72 @@ const items = [
         description: 'Quote (⌘+⇧+.)',
         icon: Icons.blockquote,
       },
-      // {
-      //   value: ELEMENT_TABLE,
-      //   label: 'Table',
-      //   description: 'Table',
-      //   icon: Icons.table,
-      // },
-      // {
-      //   value: 'ul',
-      //   label: 'Bulleted list',
-      //   description: 'Bulleted list',
-      //   icon: Icons.ul,
-      // },
-      // {
-      //   value: 'ol',
-      //   label: 'Numbered list',
-      //   description: 'Numbered list',
-      //   icon: Icons.ol,
-      // },
-      // {
-      //   value: ELEMENT_HR,
-      //   label: 'Divider',
-      //   description: 'Divider (---)',
-      //   icon: Icons.hr,
-      // },
+      {
+        value: ELEMENT_TABLE,
+        label: 'Table',
+        description: 'Table',
+        icon: Icons.table,
+      },
+      {
+        value: 'ul',
+        label: 'Bulleted list',
+        description: 'Bulleted list',
+        icon: Icons.ul,
+      },
+      {
+        value: 'ol',
+        label: 'Numbered list',
+        description: 'Numbered list',
+        icon: Icons.ol,
+      },
+      {
+        value: ELEMENT_HR,
+        label: 'Divider',
+        description: 'Divider (---)',
+        icon: Icons.hr,
+      },
     ],
   },
-  // {
-  //   label: 'Media',
-  //   items: [
-  //     {
-  //       value: ELEMENT_CODE_BLOCK,
-  //       label: 'Code',
-  //       description: 'Code (```)',
-  //       icon: Icons.codeblock,
-  //     },
-  //     {
-  //       value: ELEMENT_IMAGE,
-  //       label: 'Image',
-  //       description: 'Image',
-  //       icon: Icons.image,
-  //     },
-  //     {
-  //       value: ELEMENT_MEDIA_EMBED,
-  //       label: 'Embed',
-  //       description: 'Embed',
-  //       icon: Icons.embed,
-  //     },
-  //     {
-  //       value: ELEMENT_EXCALIDRAW,
-  //       label: 'Excalidraw',
-  //       description: 'Excalidraw',
-  //       icon: Icons.excalidraw,
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: 'Inline',
-  //   items: [
-  //     {
-  //       value: ELEMENT_LINK,
-  //       label: 'Link',
-  //       description: 'Link',
-  //       icon: Icons.link,
-  //     },
-  //   ],
-  // },
+  {
+    label: 'Media',
+    items: [
+      {
+        value: ELEMENT_CODE_BLOCK,
+        label: 'Code',
+        description: 'Code (```)',
+        icon: Icons.codeblock,
+      },
+      {
+        value: ELEMENT_IMAGE,
+        label: 'Image',
+        description: 'Image',
+        icon: Icons.image,
+      },
+      {
+        value: ELEMENT_MEDIA_EMBED,
+        label: 'Embed',
+        description: 'Embed',
+        icon: Icons.embed,
+      },
+      {
+        value: ELEMENT_EXCALIDRAW,
+        label: 'Excalidraw',
+        description: 'Excalidraw',
+        icon: Icons.excalidraw,
+      },
+    ],
+  },
+  {
+    label: 'Inline',
+    items: [
+      {
+        value: ELEMENT_LINK,
+        label: 'Link',
+        description: 'Link',
+        icon: Icons.link,
+      },
+    ],
+  },
 ];
 
 export function InsertDropdownMenu(props: DropdownMenuProps) {
