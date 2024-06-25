70511907e9a6a8e05ff4ca47a1760b272dbc4146
Selyss <99344963+Selyss@users.noreply.github.com>
2024-06-25 21:26:49 +0000

    desmos package, issue with ssr

--- src/components/plate-ui/desmos.tsx
+++ src/components/plate-ui/desmos.tsx
@@ -4,31 +4,14 @@ import { createPluginFactory } from '@udecode/plate-common';
 
 export const ELEMENT_DESMOS = 'desmos';
 
-// Declare Desmos on the window object to avoid TypeScript errors
-declare global {
-  interface Window {
-    Desmos: any;
-  }
-}
-
 const DesmosElement = () => {
   const desmosRef = useRef(null);
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src =
-      'https://www.desmos.com/api/v1.9/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6';
-    script.async = true;
-    script.onload = () => {
-      if (desmosRef.current && window.Desmos) {
-        const calculator = window.Desmos.GraphingCalculator(desmosRef.current);
-      }
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
+    if (desmosRef.current) {
+        const calculator = Desmos.GraphingCalculator(desmosRef.current);
+        calculator.setExpression({ id: 'graph1', latex: 'y=x^2' });
+    }
   }, []);
   return <div ref={desmosRef} style={{ width: '600px', height: '400px' }} />;
 };
